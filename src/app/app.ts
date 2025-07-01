8c632b144a1f4332b1204f74429ad67ddc33343f
Praktikant <Praktikant@imc.local>
2025-07-01 16:54:23 +0200

    Added header and search

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Pokedex } from './pokedex/pokedex';
+import { Search } from './search/search';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Pokedex],
+  imports: [RouterOutlet, Pokedex, Search],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
