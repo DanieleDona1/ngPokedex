865c627d04f90bd2d1d0b21a2e99b826cb6084f9
Daniele Dona <d.dona01@gmail.com>
2025-07-05 20:47:54 +0200

    Added load team pokedex

--- src/app/team-pokedex/team-pokedex.ts
+++ src/app/team-pokedex/team-pokedex.ts
@@ -6,12 +6,34 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './team-pokedex.html',
-  styleUrl: './team-pokedex.scss'
+  styleUrl: './team-pokedex.scss',
 })
 export class TeamPokedex {
-  // team: any[] = [];
+  storedTeam: any[] = [];
 
-  // ngDoCheck(): void {
-  //   this.team = JSON.parse(localStorage.getItem('pokemonTeam') || '[]');
-  // }
+  ngOnInit(): void {
+    this.loadPokemonTeam();
+  }
+
+  loadPokemonTeam(): void {
+    const teamString = localStorage.getItem('pokemonTeam');
+    this.storedTeam = teamString ? JSON.parse(teamString) : [];
+    console.log('Loaded Pokémon Team:', this.storedTeam);
+  }
+
+  addToTeam(pokemon: any, event: Event): void {
+    event.stopPropagation();
+    const team = JSON.parse(localStorage.getItem('pokemonTeam') || '[]');
+    const index = team.findIndex((p: any) => p.id === pokemon.id);
+
+    team.splice(index, 1);
+
+    localStorage.setItem('pokemonTeam', JSON.stringify(team));
+    this.loadPokemonTeam();
+  }
+
+  isInTeam(pokemon: any): boolean {
+    const team = JSON.parse(localStorage.getItem('pokemonTeam') || '[]');
+    return team.some((p: any) => p.id === pokemon.id);
+  }
 }
